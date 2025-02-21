d347037451d35c7b800edaf33411cca18ae786b1
Luca4869 <sulfuricacid2k6@gmail.com>
2025-02-21 14:48:40 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function login()
 {
   let u = username.value
   let p= pw.value
-  if (u=="Luca" && p=="KMNO4")
+  if (u=="Luca" && p=="Kalicyanide")
   { 
      window.open("home.html")
     document.getElementById(compiler).style.display="block"
