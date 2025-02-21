14f7e0fae2f14b2be5fac0deeb1011e1826fef38
Luca4869 <sulfuricacid2k6@gmail.com>
2025-02-21 19:03:13 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
  function login() {
-        let u = document.getElementById("username").value;
-        let p = document.getElementById("pw").value;
-        let errorMsg = document.getElementById("error-msg");
-        
-        errorMsg.style.display = "none"; // Ẩn lỗi trước khi kiểm tra
-        
-        if (u === "Luca" && p === "Kalicyanide") {
-            window.location.href = "home.html";
-            return false;
-        } else {
-            errorMsg.style.display = "block";
-            return false;
-        }
- }
+    let u = document.getElementById("username").value;
+    let p = document.getElementById("pw").value;
+    let errorMsg = document.getElementById("error-msg");
+    
+    errorMsg.style.display = "none"; 
+    
+    if (u === "Luca" && p === "Kalicyanide") {
+        window.location.href = "home.html";
+        return false;
+    } else {
+        errorMsg.style.display = "block";
+        return false;
+    }
+}
