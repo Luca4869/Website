59b8c7dbae2a0c1cc15b3ea44e7c051b485dd661
Luca4869 <sulfuricacid2k6@gmail.com>
2025-02-27 21:22:33 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
  function login() {
+  
+
     let u = document.getElementById("username").value;
     let p = document.getElementById("pw").value;
     let errorMsg = document.getElementById("error-msg");
     
     errorMsg.style.display = "none"; 
     
-    if (u === "Luca" && p === "KMNO4") {
+    if (u === "Luca" && p === "KCN") {
         window.location.href = "home.html";
         return false;
     } else {
         errorMsg.style.display = "block";
         return false;
     }
-}
+ }
